b8fbfc75bae2bfa9569c4488706dea1060e845f5
Alvin Galit <aroblesgalit@gmail.com>
2020-10-29 20:44:38 -0500

    Added logic for rewinding by 10 seconds

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,9 +1,23 @@
+// Video
 const video = document.querySelector('.player__video');
+// Play button
 const playBtn = document.querySelector('.player__button');
+// Progress bar
+const progressBar = document.querySelector('.progress__filled');
+// Rewind 10 seconds button
+const rewind = document.querySelector('.player__button[data-skip="-10"]');
+// Skip 25 seconds button
+const skip = document.querySelector('.player__button[data-skip="25"]');
 
+// Click events on video and play button
 playBtn.addEventListener('click', playVid);
 video.addEventListener('click', playVid);
 
+// Click event on rewind and skip
+rewind.addEventListener('click', rewindVid);
+// skip.addEventListener('click', skipVidAhead);
+
+// Play video
 function playVid() {
     if (playBtn.innerHTML === '►') {
         video.play();
@@ -12,5 +26,13 @@ function playVid() {
         video.pause();
         playBtn.innerHTML = '►';
     }
-    
+}
+
+// Rewind video by 10 seconds
+function rewindVid() {
+    if (video.currentTime <= 10) {
+        video.currentTime = 0;
+    } else {
+        video.currentTime -= 10;
+    }
 }
