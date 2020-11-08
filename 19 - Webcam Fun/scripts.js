205cf56739ea7bb185dff8febe266030c7e3371f
Alvin Galit <aroblesgalit@gmail.com>
2020-11-07 19:19:52 -0600

    Created function that paints the video to the canvas

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -5,7 +5,7 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
-// Get video
+// Get video stream
 function getVideo() {
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         .then(localMediaStream => {
@@ -13,6 +13,20 @@ function getVideo() {
             video.play();
         })
         .catch(err => console.error(`OH NO!!! ${err}`));
+}
+
+// Take frame from video and add to canvas
+function paintToCanvas() {
+    // Get width and height of video
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    // Set width and height of canvas to that of the video
+    canvas.width = width;
+    canvas.height = height;
+    // Every 16 ms, take an image from webcam and paint to canvas
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
 }
 
 getVideo();
