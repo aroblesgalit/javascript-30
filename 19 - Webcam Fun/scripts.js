76d491782679c9ddc891dc136f419b3563a0b770
Alvin Galit <aroblesgalit@gmail.com>
2020-11-07 19:12:43 -0600

    Created function to get the local video stream and render on the DOM

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,18 @@
+// Target necessary elements
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+// Get video
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch(err => console.error(`OH NO!!! ${err}`));
+}
+
+getVideo();
