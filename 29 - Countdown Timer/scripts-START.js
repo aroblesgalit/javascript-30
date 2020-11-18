1f18f95e4880f8d6bbd4fb68fa6cba0525208b29
Alvin Galit <aroblesgalit@gmail.com>
2020-11-17 22:06:12 -0600

    Created displayEndTime to grab the end time in hour and minutes and display it to the DOM

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
     const now = Date.now(); // Current time in milliseconds
     const then = now + (seconds * 1000); // Current time + the seconds
-    displayTimeLeft(seconds); // Display time
+    displayTimeLeft(seconds); // Display current time
+    displayEndTime(then); // Display end time
 
     // Display amount time left every second
     countdown = setInterval(() => {
@@ -20,8 +22,20 @@ function timer(seconds) {
 }
 
 function displayTimeLeft(seconds) {
+    // Get minutes and seconds
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
+    // Display to the DOM
     const display = `${minutes}:${remainderSeconds < 10 ? '0' + remainderSeconds : remainderSeconds}`;
+    document.title = display; // Displays on window's tab
     timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    // Get hour and minutes
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    // Display to the DOM
+    endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' + minutes : minutes}`;
 }
