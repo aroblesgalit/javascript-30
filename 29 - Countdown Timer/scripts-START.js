9023a15845914e9e7ce63c50c02f9bf73e6aa8fe
Alvin Galit <aroblesgalit@gmail.com>
2020-11-17 21:51:48 -0600

    Updated displayTimeLeft to calculate the minutes and seconds left and displayed it

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,15 +1,16 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
 
 function timer(seconds) {
     const now = Date.now(); // Current time in milliseconds
     const then = now + (seconds * 1000); // Current time + the seconds
     displayTimeLeft(seconds); // Display time
-    
+
     // Display amount time left every second
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         // Check if we should stop it
-        if (secondsLeft <= 0) {
+        if (secondsLeft < 0) {
             clearInterval(countdown);
             return;
         }
@@ -19,5 +20,8 @@ function timer(seconds) {
 }
 
 function displayTimeLeft(seconds) {
-    console.log(seconds);
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' + remainderSeconds : remainderSeconds}`;
+    timerDisplay.textContent = display;
 }
